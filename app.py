d28f77abc541984bd5d0142d6f2f0be12cbd6698
fenocarlie609 <fenocarlie69@gmail.com>
2026-05-17 10:22:38 +0300

    test commit 8080 to 9090

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ def hello_world():
     return 'Mon application DevOps fonctionne sur le port 8081 et mise a jour !'
 
 if __name__ == '__main__':
-    # L'application écoute sur le port 8080 à l'intérieur du conteneur
-    app.run(host='0.0.0.0', port=8080)
+    # L'application écoute sur le port 9090 à l'intérieur du conteneur
+    app.run(host='0.0.0.0', port=9090)
